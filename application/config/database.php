78f10831b2b2a04ebf4ff834d05ffd6e26b642eb
Benjamin Sharf <Ben@Benjamins-MacBook-Air.local>
2016-05-22 17:51:23 -0700

    ben

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
+$db['default']['hostname'] = '172.31.28.115';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'root';
+$db['default']['password'] = 'Hockey1!';
 $db['default']['database'] = 'login_and_registration';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
